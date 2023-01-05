e356c06dcf2cd5a9ed991038c2282e6c320bdf9e
THANGAIYA18 <thangaiya18@gmail.com>
2023-01-05 14:24:13 +0530

    new code

---
+++ src/test/java/com/gitclass/Git.java
@@ -0,0 +1,16 @@
+package com.gitclass;
+
+public class Git {
+
+private void ajith() {
+System.out.println("1");
+}
+private void vijay() {
+	System.out.println("2");
+
+}
+private void surya() {
+	System.out.println("3");
+
+}
+}
