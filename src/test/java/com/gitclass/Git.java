4703aa13e9321a7c47f4eb25087ee4d94d00d13b
Arunraj718 <arun.bk718@gmail.com>
2023-01-06 11:40:29 +0530

    u[adyfgk

--- src/test/java/com/gitclass/Git.java
+++ src/test/java/com/gitclass/Git.java
@@ -2,15 +2,30 @@ package com.gitclass;
 
 public class Git {
 
-private void ajith() {
-System.out.println("1");
-}
-private void vijay() {
-	System.out.println("2");
+	private void ajith() {
+		System.out.println("1");
+	}
 
-}
-private void surya() {
-	System.out.println("3");
+	private void vijay() {
+		System.out.println("2");
 
-}
+	}
+
+	private void surya() {
+		System.out.println("3");
+	}
+
+	public void Cricket() {
+		System.out.println("Dhoni");
+
+	}
+
+	public void Football() {
+		System.out.println("Ronaldo");
+	}
+
+	public void Basketball() {
+		System.out.println("James");
+
+	}
 }
